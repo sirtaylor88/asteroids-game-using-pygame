5e715024add6eabb1b43a5a695cb59acce1108d1
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2024-08-27 07:05:37 +0200

    🔧 Setup pygame.

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Main program"""
+
+import pygame
+
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000")  # fill black color
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
